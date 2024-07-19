899915957957cb7f854559e3075f8142eae71d87
Keriafe-Richard <89256650+Keriafe-Richard@users.noreply.github.com>
2024-07-19 15:23:39 +0100

    Add files via upload

---
+++ Train Passenger Counter js/main.js
@@ -0,0 +1,17 @@
+let countEl = document.getElementById("count-el")
+let count = 0
+
+let saveEl = document.getElementById("save-el")
+
+function increment() {
+    count +=1
+    countEl.textContent = count
+}
+
+function save() {
+    let value = count + "  - "
+    saveEl.textContent += value
+    countEl.textContent = 0
+    count = 0
+}
+
